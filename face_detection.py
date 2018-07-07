b1473ebbc0a1cf85bd1d60fdce35ed7b2491878c
Hunter Johnson <hhjohnso@indiana.edu>
2018-07-06 22:52:06 -0400

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -36,19 +36,53 @@ def face_detected():
     #Look for faces in the image using the loaded cascade file
         faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 	
-        print("Found "+str(len(faces))+" face(s)")
+        def face_detected():
+    found = False
+    start = time.time()
+    end = 0
+    diff = 0
+    while(found != True or diff <= 30.0): #give the function 30 seconds to find a face - can take a bit depending on distance among other factors
+        #Create a memory stream so photos doesn't need to be saved in a file
+        stream = io.BytesIO()
 
-    #Draw a green rectangle around every found face, this color can be easily changed by modifying the RGB params (R,G,B)
+        #Get the picture (low resolution, so it should be quite fast)
+        #Here you can also specify other parameters (e.g.:rotate the image)
+        with picamera.PiCamera() as camera:
+            camera.resolution = (320, 240)
+            camera.capture(stream, format='jpeg')
+
+    #Convert the picture into a numpy array
+        buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
+
+    #Now creates an OpenCV image
+        image = cv2.imdecode(buff, 1)
+
+    #Load a cascade file for detecting faces
+        face_cascade = cv2.CascadeClassifier('/home/pi/Documents/C421/frdoor_project/haarcascade_frontalface_default_2.xml')
+
+    #Convert to grayscale
+        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    #Look for faces in the image using the loaded cascade file
+        faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+        print("Found "+str(len(faces))+" face(s)")
+        image_faces = []
+    #Draw a rectangle around every found face
         for (x,y,w,h) in faces:
             cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+            image_faces.append(image[y:(y+h),x:(x+w)]) # this line of code crops the image to keep only the bounded face
 
         if(len(faces) >= 1):
             #save result image
-            x = rand.randint(1,1000)
+            x = rand.randint(1000,1200)
             s = str(x)
             result = 'result' + s + '.jpg'
             cv2.imwrite(result,image)
+            for i, face in enumerate(image_faces):
+                cv2.imwrite("face-" + s + ".jpg", face)
             found = True
+            camera.close()
             return True
         end = time.time()
         diff = end - start
